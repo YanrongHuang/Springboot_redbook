bceea31d3f5a430199997e648300ebe093054291
yanrong huang <huangyanr98@gmail.com>
2023-02-06 12:48:56 -0500

    post_CRUD

---
+++ redbook/src/main/java/com/chuwa/redbook/controller/PostController.java
@@ -0,0 +1,47 @@
+package com.chuwa.redbook.controller;
+
+import com.chuwa.redbook.payload.PostDTO;
+import com.chuwa.redbook.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/posts")
+public class PostController {
+    @Autowired
+    private PostService postService;
+
+    @PostMapping
+    public ResponseEntity<PostDTO> savePost(@RequestBody PostDTO postDTO){
+        PostDTO postResponse = postService.createPost(postDTO);
+
+        return new ResponseEntity<>(postResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<PostDTO> getAllPosts(){
+        return postService.getAllPost();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDTO> getPostById(@PathVariable(name = "id") long id) {
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDTO> updatePostById(@RequestBody PostDTO postDto, @PathVariable(name = "id") long id) {
+        PostDTO postResponse = postService.updatePost(postDto, id);
+        return new ResponseEntity<>(postResponse, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id) {
+        postService.deletePostById(id);
+        return new ResponseEntity<>("Post entity deleted successfully.", HttpStatus.OK);
+    }
+
+}
---
+++ redbook/src/main/java/com/chuwa/redbook/service/PostService.java
@@ -0,0 +1,17 @@
+package com.chuwa.redbook.service;
+
+import com.chuwa.redbook.payload.PostDTO;
+
+import java.util.List;
+
+public interface PostService {
+    PostDTO createPost(PostDTO postDTO);
+
+    List<PostDTO> getAllPost();
+
+    PostDTO getPostById(long id);
+
+    PostDTO updatePost(PostDTO postDTO,long id);
+
+    void deletePostById(long id);
+}
