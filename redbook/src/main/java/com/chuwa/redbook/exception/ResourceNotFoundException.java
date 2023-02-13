5405e1fad1d8e37334ea9229ca2e6ffa2ba9882c
yanrong huang <huangyanr98@gmail.com>
2023-02-13 15:04:42 -0500

    04_comment

--- redbook/src/main/java/com/chuwa/redbook/exception/ResourceNotFoundException.java
+++ redbook/src/main/java/com/chuwa/redbook/exception/ResourceNotFoundException.java
@@ -1,8 +1,9 @@
 package com.chuwa.redbook.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
     private String resourceName;
     private String fieldName;
